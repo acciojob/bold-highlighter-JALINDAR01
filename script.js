11c55702b93837baf787afddc2e8addb9369ffbc
JALINDAR01 <134129652+JALINDAR01@users.noreply.github.com>
2023-09-14 14:18:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function highlight() {
     //Write your code here
-
+	var boldWords=document.querySelectorALL('strong')
+	boldWords.forEach(function (word) {
+		word.style.color='green';
+	});
 
 }
 
 
 function return_normal() {
     //Write your code here
-
-    
+	var boldWords=document.querySelectorALL('strong')
+    boldWords.forEach(function (word) {
+    	word.style.color='black';
+    });
 }
